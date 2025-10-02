caae84c3a74dab97d81ed498d260cea51c84f6e4
axowatl <117409439+axowatl@users.noreply.github.com>
2025-10-02 10:35:44 -0500

    push

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let notif = new Notification("test");
 
 notif.onerror = (e) => {
     console.log("error");
+    let toast = document.createElement(HTMLDivElement);
+    toast.style = "position: fixed; bottom: 20px; right: 20px; z-index: 1000;";
+    toast.innerHTML = `<p>${message}</p>`;
 };
 
 notif.onshow = (e) => {
