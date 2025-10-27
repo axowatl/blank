99216d3834c868765a07c9ba86ab9aa961a81a51
axowatl <117409439+axowatl@users.noreply.github.com>
2025-10-27 10:24:41 -0500

    push

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const width = canvas.width;
 const height = canvas.height;
+canvas.width = width;
+canvas.height = height;
 const tileSize = 50;
 
 for (let x = 0; x < width; x += tileSize) {
