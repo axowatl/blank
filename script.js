8748e71bc9411b1f6552d02539c1acbb3e180c9d
axowatl <117409439+axowatl@users.noreply.github.com>
2025-10-27 10:47:07 -0500

    push

--- script.js
+++ script.js
@@ -11,10 +11,54 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 console.log(`(${width}, ${height})`);
-const tileSize = 10;
+const tileSize = 50;
+const tileCountX = Math.floor(width / tileSize);
+const tileCountY = Math.floor(height / tileSize);
+const cells = [];
 
-for (let x = 0; x < width; x += tileSize) {
-    for (let y = 0; y < height; y += tileSize) {
-        ctx.strokeRect(x, y, tileSize, tileSize);
+class Cell {
+    /**
+     * @param {number} x 
+     * @param {number} y 
+     */
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.visted = false;
+        this.walls = { left: true, up: true, right: true, down: true };
+    }
+
+    render() {
+        const x = this.x * tileSize;
+        const y = this.y * tileSize
+        ctx.beginPath();
+        if (this.walls.left) {
+            ctx.moveTo(x, y);
+            ctx.lineTo(x, y + tileSize);
+        }
+        if (this.walls.up) {
+            ctx.moveTo(x, y);
+            ctx.lineTo(x + tileSize, y);
+        }
+        if (this.walls.right) {
+            ctx.moveTo(x + tileSize, y);
+            ctx.lineTo(x + tileSize, y + tileSize);
+        }
+        if (this.walls.down) {
+            ctx.moveTo(x, y + tileSize);
+            ctx.lineTo(x + tileSize, y + tileSize);
+        }
+        ctx.stroke();
+        ctx.closePath();
     }
 }
+
+for (let x = 0; x < tileCountX; x++) {
+    for (let y = 0; y < tileCountY; y++) {
+        cells.push(new Cell(x, y));
+    }
+}
+
+cells.forEach((element) => {
+    element.render();
+});
