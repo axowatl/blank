269edead2e00bcca4a9aa1fe72586c4b223cb14f
axowatl <117409439+axowatl@users.noreply.github.com>
2025-10-27 10:21:49 -0500

    push

---
+++ script.js
@@ -0,0 +1,17 @@
+/**
+ * @type {HTMLCanvasElement}
+ */
+const canvas = document.getElementById("canvas");
+/**
+ * @type {CanvasRenderingContext2D}
+ */
+const ctx = canvas.getContext("2d");
+const width = canvas.width;
+const height = canvas.height;
+const tileSize = 50;
+
+for (let x = 0; x < width; x += tileSize) {
+    for (let y = 0; y < height; y += tileSize) {
+        ctx.strokeRect(x, y, tileSize, tileSize);
+    }
+}
